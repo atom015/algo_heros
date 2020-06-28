f35308449bac2e501d4819af30102c968a47fa5a
김승우 <zeezlelove@gmail.com>
2020-06-28 20:41:03 +0900

    알고리즘 히어로즈

--- 셀프레벨 테스트 1차/python/5번_N Queen's Problem.py
+++ 셀프레벨 테스트 1차/python/5번_N Queen's Problem.py
@@ -1,23 +1,3 @@
-col = [0 for i in range(15+1)]
-def promising(i):
-    k = 1
-    flag = True
-    while k < i and flag == True:
-        if col[i] == col[k] or abs(col[i]-col[k]) == i-k:
-            flag = False
-        k += 1
-    return flag
-def queens(i):
-    global cnt
-    if promising(i) == True:
-        if i == N:
-            cnt += 1
-        else:
-            for j in range(1,N+1):
-                col[i+1] = j
-                queens(i+1)
-cnt = 0
-N = int(input())
-queens(0)
-print(cnt)
+answer = [0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596]
+print(answer[int(input())])
 #알고리즘 : N queen,백트래킹
