db46f9f1fb711c8a51a8105ace131853555b563b
김승우 <zeezlelove@gmail.com>
2020-06-28 20:40:58 +0900

    알고리즘 히어로즈

--- 셀프레벨 테스트 1차/python/3번_계단 오르기.py
+++ 셀프레벨 테스트 1차/python/3번_계단 오르기.py
@@ -1,5 +1,6 @@
-N = int(input())
-dp = [1,1]+[0 for i in range(N+1)]
-for i in range(2,N+1):
-    dp[i] = dp[i-2]+1
-print(dp)
+n = int(input())
+dp = [1,1]+[0 for i in range(n)]
+for i in range(1,n+1):
+    dp[i] = dp[i-1]+dp[i-2]
+print(dp[n])
+#알고리즘 : dp
