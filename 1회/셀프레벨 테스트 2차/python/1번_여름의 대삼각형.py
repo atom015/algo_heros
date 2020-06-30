2399634e19a9d47f5bd2ab904956a8fa18875a49
김승우 <zeezlelove@gmail.com>
2020-06-30 15:20:41 +0900

    알고리즘 히어로즈

---
+++ 1회/셀프레벨 테스트 2차/python/1번_여름의 대삼각형.py
@@ -0,0 +1,6 @@
+x1,y1 = map(int,input().split())
+x2,y2 = map(int,input().split())
+x3,y3 = map(int,input().split())
+ans = abs((x1*y2+x2*y3+x3*y1)-(x1*y3+x3*y2+x2*y1))/2
+print(str(ans)+"0")
+#알고리즘 : 수학
