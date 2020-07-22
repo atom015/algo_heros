f797b2638ebd0b3a4afd001d4cc98df53401d9e6
김승우 <zeezlelove@gmail.com>
2020-07-22 21:11:29 +0900

    알고리즘 히어로즈

---
+++ 2회/필수강좌 코드/앵무새 꼬꼬.py
@@ -0,0 +1,7 @@
+for i in range(int(input())):
+    s = input()
+    ans = ""
+    for i in s:
+        if i.lower() in ("a","e","i","o","u"):
+            ans += i
+    print("???" if len(ans) == 0 else ans)
