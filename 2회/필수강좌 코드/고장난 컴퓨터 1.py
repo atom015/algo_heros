5fd2ed50ef0e935524945e3bdecd1bec14732238
김승우 <zeezlelove@gmail.com>
2020-08-13 01:06:20 +0900

    알고리즘 히어로즈

--- 2회/필수강좌 코드/고장난 컴퓨터 1.py
+++
@@ -1,9 +0,0 @@
-N,c = map(int,input().split())
-arr = list(map(int,input().split()))
-ans = 1
-for i in range(N-1):
-    if arr[i+1]-arr[i] <= c:
-        ans += 1
-    else:
-        ans = 1
-print(ans)
